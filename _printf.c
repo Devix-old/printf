50fa50a50a0112ac39dbb4cfff5061e19b3c6e4e
devix-old <mohamed.elgharef@gmail.com>
2023-04-22 15:32:03 -0700

    ss

--- _printf.c
+++ _printf.c
@@ -1,50 +1,47 @@
 #include <stdarg.h>
 #include "main.h"
 /**
- * _printf - custom printf function
- * @format: format string
+ * _printf - Custom printf function
+ * @format: Format string
  *
- * Return: number of characters printed
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    int c, count;
-    char *s;
-    va_list args;
-    count = 0;
-
-    va_start(args, format);
-
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-                c = va_arg(args, int);
-                _putchar(c);
-                format++;
-                count++;
-            }
-            if (*format == 's')
-            {
-                format++;
-                s = va_arg(args, char *);
-                while (*s != '\0')
-                {
-                    _putchar(*s);
-                    s++;
-                    count++;
-                }
-            }
-        }
-
-        _putchar(*format);
-        format++;
-        count++;
-    }
-    va_end(args);
-    return (count);
+int c, count;
+char *s;
+va_list args;
+count = 0;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+c = va_arg(args, int);
+_putchar(c);
+format++;
+count++;
+}
+if (*format == 's')
+{
+format++;
+s = va_arg(args, char *);
+while (*s != '\0')
+{
+_putchar(*s);
+s++;
+count++;
+}
+}
+}
+_putchar(*format);
+format++;
+count++;
+}
+va_end(args);
+return (count);
 }
 

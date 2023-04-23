bf81e3008422927e62403d9827c64a7172bfae06
devix-old <mohamed.elgharef@gmail.com>
2023-04-22 19:35:40 -0700

    function with putchar

--- _printf.c
+++ _printf.c
@@ -1,68 +1,47 @@
-#include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
-#include <string.h>
 
 /**
- * _printf - produces output according to a format
- * @format: format string
- * @...: variable number of arguments
+ * _printf - Custom printf function
+ * @format: Format string
  *
- * Return: number of characters printed (excluding the null byte)
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed_chars = 0;
-	int write_chars = 0;
-	char c;
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					c = va_arg(args, int);
-					write_chars = write(1, &c, 1);
-					break;
-				case 's':
-					write_chars = write(1, va_arg(args, char *), strlen(va_arg(args, char *)));
-					break;
-				case '%':
-					c = '%';
-					write_chars = write(1, &c, 1);
-					break;
-				default:
-					c = '%';
-					write_chars = write(1, &c, 1);
-					c = *format;
-					write_chars += write(1, &c, 1);
-					break;
-			}
-			if (write_chars == -1)
-				return (-1);
-			printed_chars += write_chars;
-		}
-		else
-		{
-			write_chars = write(1, format, 1);
-			if (write_chars == -1)
-				return (-1);
-			printed_chars += write_chars;
-		}
-		format++;
-	}
-
-	va_end(args);
-
-	return (printed_chars);
+int c, count;
+char *s;
+va_list args;
+count = 0;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+c = va_arg(args, int);
+_putchar(c);  
+format++;
+count++;
 }
-
+if (*format == 's')
+{
+format++;
+s = va_arg(args, char *);
+while (*s != '\0')
+{
+_putchar(*s); 
+s++;
+count++;
+}
+}
+}
+_putchar(*format);
+format++;
+count++;
+}
+va_end(args);
+return (count);
+}

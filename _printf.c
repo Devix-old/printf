aa6b215453a495bb7070231adf1313500b4cb80a
devix-old <mohamed.elgharef@gmail.com>
2023-04-22 18:46:21 -0700

    putchar changed by write

--- _printf.c
+++ _printf.c
@@ -1,57 +1,95 @@
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include "main.h"
+
 /**
- * _printf - Custom printf function
- * @format: Format string
+ * _putchar - writes a character to stdout
+ * @c: character to print
  *
- * Return: Number of characters printed
+ * Return: On success 1, on error -1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ * @...: variable number of arguments
+ *
+ * Return: number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-int c, count;
-char *s;
-va_list args;
-count = 0;
-va_start(args, format);
-if (format == NULL || *format == '\0')
+	va_list args;
+	int printed_chars = 0;
+	char c;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					c = va_arg(args, int);
+					_putchar(c);
+					printed_chars++;
+					break;
+				case 's':
+					printed_chars += _print_str(va_arg(args, char *));
+					break;
+				case '%':
+					_putchar('%');
+					printed_chars++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					printed_chars += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			printed_chars++;
+		}
+		format++;
+	}
+
+	va_end(args);
+
+	return (printed_chars);
+}
+
+/**
+ * _print_str - prints a string
+ * @str: string to print
+ *
+ * Return: number of characters printed
+ */
+int _print_str(char *str)
 {
-return (-1);
+	int i;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	for (i = 0; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (i);
 }
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-if (*format == '%')
-{
-write(1, "%", 1);
-format++;
-count++;
-}
-if (*format == 'c')
-{
-c = va_arg(args, int);
-write(1, &c, 1);
-format++;
-count++;
-}
-if (*format == 's')
-{
-format++;
-s = va_arg(args, char *);
-while (*s != '\0')
-{
-write(1, s, 1);
-s++;
-count++;
-}
-}
-}
-write(1, format, 1);
-format++;
-count++;
-}
-va_end(args);
-return (count);
-}
+

cb0c2fba84c034ef3f768c6894540bb9e416ab04
devix-old <mohamed.elgharef@gmail.com>
2023-04-23 19:09:04 -0700

    function with putchar

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+void convert_to_binary(va_list args, int *count);
 void process_format(va_list args, const char **format, int *count);
 int _printf(const char *format, ...);
 int _putchar(char c);

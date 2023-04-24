cd0d01204a5b57ac97849e0a042030bd4cef6a7b
Devix-old <125441805+Devix-old@users.noreply.github.com>
2023-04-24 02:28:54 +0000

    Update process_format.c

--- process_format.c
+++ process_format.c
@@ -1,29 +1,36 @@
 #include "main.h"
 #include <stdarg.h>
+/**
+ * process_format - Process the format specifier and call appropriate functions
+ * @args: va_list containing the argument to print
+ * @format: Pointer to the format specifier
+ * @count: Pointer to the count of characters printed
+ * Return: void
+ */
 void process_format(va_list args, const char **format, int *count)
 {
-  switch (**format)
-  {
-  case 'c':
-    print_char(args, count);
-    break;
-  case 's':
-    print_string(args, count);
-    break;
-  case 'd':
-  case 'i':
-    print_integer(args, count);
-    break;
-  case 'b':
-    convert_to_binary(args, count);
-    break;
-  case '%':
-    print_percent(args, count);
-    break;
-  default:
-    _putchar('%');
-    _putchar(**format);
-    (*count) += 2;
-    break;
-  }
+switch (**format)
+{
+case 'c':
+print_char(args, count);
+break;
+case 's':
+print_string(args, count);
+break;
+case 'd':
+case 'i':
+print_integer(args, count);
+break;
+case 'b':
+convert_to_binary(args, count);
+break;
+case '%':
+print_percent(args, count);
+break;
+default:
+_putchar('%');
+_putchar(**format);
+(*count) += 2;
+break;
+}
 }

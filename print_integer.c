6b3861ea559446d35691f9b3d3101246e1470c4e
Devix-old <125441805+Devix-old@users.noreply.github.com>
2023-04-24 23:01:47 +0000

    Update print_integer.c

--- print_integer.c
+++ print_integer.c
@@ -7,42 +7,42 @@
  */
 void print_integer(va_list args, int *count)
 {
-  long int n = va_arg(args, long int);
-  int i;
-  char buffer[32];
-  int len = 0;
-  if (n == 0)
-  {
-    _putchar('0');
-    (*count)++;
-    return;
-  }
-  else
-  {
-    int negative = 0;
-    unsigned int num;
-    if (n < 0)
-    {
-      negative = 1;
-      num = (unsigned int)(-n);
-    }
-    else
-    {
-      num = (unsigned int)n;
-    }
-    while (num > 0)
-    {
-      buffer[len++] = num % 10 + '0';
-      num /= 10;
-      (*count)++;
-    }
-    if (negative)
-    {
-      buffer[len++] = '-';
-    }
-  }
-  for (i = len - 1; i >= 0; i--)
-  {
-    _putchar(buffer[i]);
-  }
+long int n = va_arg(args, long int);
+int i;
+char buffer[32];
+int len = 0;
+if (n == 0)
+{
+_putchar('0');
+(*count)++;
+return;
+}
+else
+{
+int negative = 0;
+unsigned int num;
+if (n < 0)
+{
+negative = 1;
+num = (unsigned int)(-n);
+}
+else
+{
+num = (unsigned int)n;
+}
+while (num > 0)
+{
+buffer[len++] = num % 10 + '0';
+num /= 10;
+(*count)++;
+}
+if (negative)
+{
+buffer[len++] = '-';
+}
+}
+for (i = len - 1; i >= 0; i--)
+{
+_putchar(buffer[i]);
+}
 }

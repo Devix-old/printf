69d532c3117abe52151b1ac43aceade42a964769
devix-old <mohamed.elgharef@gmail.com>
2023-04-24 14:16:07 -0700

    function with putchar

--- print_integer.c
+++ print_integer.c
@@ -1,27 +1,20 @@
 #include "main.h"
 #include <stdarg.h>
 #include <limits.h>
-#include <stdlib.h>
-
 /**
- * print_integer - prints an integer
- * @args: list of arguments
- * @count: pointer to the character count
- * Return: void
- */
+* print_integer - prints an integer
+* @args: list of arguments
+* @count: pointer to the character count
+* Return: void
+*/
 void print_integer(va_list args, int *count)
 {
-int n = va_arg(args, int);
+long int n = va_arg(args, int);
 int i;
-char *buffer;
+char buffer[32];
 int len = 0;
 if (n == 0)
 {
-buffer = malloc(sizeof(char));
-if (buffer == NULL)
-{
-return;
-}
 buffer[len++] = '0';
 }
 else
@@ -34,33 +27,17 @@ n = -n;
 }
 while (n > 0)
 {
-buffer = realloc(buffer, (len + 1) * sizeof(char)); 
-if (buffer == NULL)
-{
-free(buffer);
-return;
-}
 buffer[len++] = n % 10 + '0';
 n /= 10;
 (*count)++;
 }
 if (negative)
 {
-buffer = realloc(buffer, (len + 1) * sizeof(char)); 
-if (buffer == NULL)
-{
-free(buffer);
-return;
-}
 buffer[len++] = '-';
 }
 }
-
-    for (i = len - 1; i >= 0; i--)
-    {
-        _putchar(buffer[i]);
-    }
-
-    free(buffer);
+for (i = len - 1; i >= 0; i--)
+{
+_putchar(buffer[i]);
 }
-
+}

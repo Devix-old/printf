dddd3368049e2cc9f61fb11bbe1c8ee2f91d842a
devix-old <mohamed.elgharef@gmail.com>
2023-04-23 19:13:19 -0700

    function with putchar

--- process_format.c
+++ process_format.c
@@ -14,6 +14,9 @@ void process_format(va_list args, const char **format, int *count)
   case 'i':
     print_integer(args, count);
     break;
+  case 'b':
+    convert_to_binary(args, count);
+    break;
   case '%':
     print_percent(args, count);
     break;
